e4e28bdf6ea2fc911e3aee6b3002606684d23ec5
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-08-31 00:00:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" }
+]
 
+function superbowlWin(record) {
+    let winner = record.find(e => e.result === "W")
+    if (winner) {
+        return winner.year
+    } else {
+        return undefined
+    }
+
+}
